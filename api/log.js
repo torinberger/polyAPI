4fb207c7f62b74aaf645f08df20ba87827139531
torinberger <torinberger@torinberger.com>
2021-01-20 08:40:40 +1100

    Finished the API

--- api/log.js
+++ api/log.js
@@ -35,6 +35,18 @@ logRouter.post('/get', async (ctx, next) => {
   ctx.body = logsInDB;
 });
 
+/* params:
+userName,
+schedule,
+adapted,
+day,
+moods,
+awakeDifficulty,
+oversleepTime,
+attachment,
+loggedFrom,
+loggedTo
+*/
 logRouter.post('/entries/get', async (ctx, next) => {
   // extract POST parameters
   const postData = ctx.request.body;
@@ -50,18 +62,116 @@ logRouter.post('/entries/get', async (ctx, next) => {
           loggedTo } = postData;
   console.log(postData);
 
+  // find user in database
   const userInDB = await database.users.find({ userName }).exec();
   if (userInDB) {
     postData = util.cleanObj(postData); // remove all empty params
-    let logsInDB = await database.logs.find(postData).exec();
-    // FIND ENTRIES NOT LOGS
-    
+    // find all logs associated with user
+    let logsInDB = await database.logs.find({userName, schedule}).exec();
+    // delete post data that does not match with the database format
+    delete postData.userName;
+    delete postData.schedule;
+    delete postData.loggedFrom;
+    delete postData.loggedTo;
+
+    let entries = {};
+
+    // if the user has any associated logs
+    if (logsInDB.length > 0) {
+      let k = -1;
+      for (var n = 0; n < userInDB.historicSchedules.length; n++) {
+        if(userInDB.historicSchedules[n].name == schedule) {
+          k += 1;
+          // chech if adaptation status matters if so, remove if not adapted
+          if (adapted !== undefined && !userInDB.historicSchedules[n].adaptDate) {
+            logsInDB.splice(k, 1);
+          }
+        }
+      }
+
+      for (var i = 0; i < logsInDB.length; i++) {
+
+        // find entries
+        let entriesInLogs = logsInDB[i].entries;
+
+        for (var n = 0; n < entriesInLogs.length; n++) {
+          if (loggedFrom && loggedTo) {
+            if (!(loggedFrom <= entriesInLogs[n].adaptDate && entriesInLogs[n].adaptDate <= loggedTo)) {
+              entriesInLogs.splice(i, 1);
+              continue;
+            }
+          } if (day !== undefined) {
+            if (entriesInLogs[n].day != day) {
+              entriesInLogs.splice(i, 1);
+              continue;
+            }
+          } if (moods !== undefined) {
+            if (entriesInLogs[n].moods != moods) {
+              entriesInLogs.splice(i, 1);
+              continue;
+            }
+          } if (awakeDifficulty !== undefined) {
+            if (entriesInLogs[n].awakeDifficulty != awakeDifficulty) {
+              entriesInLogs.splice(i, 1);
+              continue;
+            }
+          } if (oversleepTime !== undefined) {
+            // delete if overlseep time is greater than time provided
+            if (entriesInLogs[n].oversleepTime < oversleepTime) {
+              entriesInLogs.splice(i, 1);
+              continue;
+            }
+          } if (attachment !== undefined) {
+            // check if attachment exists
+            if (!entriesInLogs[n].attachment) {
+              entriesInLogs.splice(i, 1);
+              continue;
+            }
+          }
+        }
+
+        entries[`${logsInDB.schedule}-attempt${logsInDB.attempt}`] = entriesInLogs;
+      }
+    }
   } else {
     ctx.status = 401;
   }
 
   ctx.status = 200;
-  ctx.body = logsInDB;
+  ctx.body = entries;
+});
+
+logRouter.post('/summary', async (ctx, next) => {
+  // extract POST parameters
+  const postData = ctx.request.body;
+  const { userName,
+          schedule } = postData;
+
+  let historics = {};
+
+  let userInDB = await database.users.find({ userName }).exec();
+  if(userInDB) {
+    for (var i = 0; i < userInDB.historicSchedules.length; i++) {
+      if(userInDB.historicSchedules[i].name == schedule) {
+        if(historics[schedule]) {
+          historics[schedule].attempts += 1;
+          if (userInDB.historicSchedules[i].adaptDate !== undefined) {
+            historics[schedule].adaptations += 1;
+          }
+        } else {
+          historics[schedule].attempts = 1;
+          if (userInDB.historicSchedules[i].adaptDate !== undefined) {
+            historics[schedule].adaptations = 1;
+          } else {
+            historics[schedule].adaptations = 0;
+          }
+        }
+      }
+    }
+  }
+
+  ctx.status = 200;
+  ctx.body = historics;
 });
 
 module.exports = logRouter;
